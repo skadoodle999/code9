c0cccaef8deae3b382a567fd0a34fefa2291d823
Levi9 <uros.paunovic1010@gmail.com>
2023-10-28 12:54:29 +0200

    Wordpress push action

--- terraform/wordpress/providers.tf
+++ terraform/wordpress/providers.tf
@@ -12,7 +12,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "code9-vlada-state-file-s3-bucket"
+    bucket = "mojbaket2"
     key    = "terraform/wordpress"
     region = "us-east-1"
   }
